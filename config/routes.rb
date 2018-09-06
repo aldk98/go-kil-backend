d2235c52d29acffcfb66f40ea6a094c7a3e37657
loem <arlenluman@gmail.com>
2018-09-07 02:14:00 +0700

    add driver model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   scope :api, defaults: {format: :json} do
     devise_for :customers
+    devise_for :drivers
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
