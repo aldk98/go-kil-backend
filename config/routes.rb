acc6234df92f4a77284479387f3ddba5c1d9ba89
loem <arlenluman@gmail.com>
2018-09-10 23:36:18 +0700

    add driver test

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,18 @@ Rails.application.routes.draw do
     devise_for :customers, controllers: {
       sessions: 'customers/sessions'
     }
+    devise_for :drivers, controllers: {
+      sessions: 'drivers/sessions'
+    }
 
     devise_scope :customer do
       get 'customers/current', to: 'customers/sessions#show'
     end
 
-    devise_for :drivers
+    devise_scope :driver do
+      get 'drivers/current', to: 'drivers/sessions#show'
+    end
+    
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Driver, type: :model do
+    it "has a valid factory" do
+        expect(FactoryBot.build(:driver)).to be_valid
+    end
   
+    it "it valid with an email and password" do
+      expect(FactoryBot.build(:driver)).to be_valid
+    end
+  
+    it "is not valid without an email" do
+      user1 = FactoryBot.build(:driver, email: nil)
+      expect(user1).to_not be_valid
+    end
+  
+    it "is not valid without a password" do
+      user1 = FactoryBot.build(:driver, password: nil)
+      expect(user1).to_not be_valid
+    end
+  
+    it "is invalid with a duplicate email" do
+      user1 = FactoryBot.create(:driver, email: "blabla@gmail.com")
+      user2 = FactoryBot.build(:driver, email: "blabla@gmail.com")
+      user2.valid?
+      expect(user2.errors[:email]).to include("has already been taken")
+    end
+   
 end
